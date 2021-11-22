72312374f9b58b921da65354148db4ec19f6d55c
Dimitris Markou <damarkou@csd.auth.gr>
2021-11-22 22:25:29 +0200

    Commit Quiz Game

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import GUI.Intro;
+import java.io.FileNotFoundException;
+
+/** This class creates an object in main and calls play*/
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        new Intro();
+    }
+}
